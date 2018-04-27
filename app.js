04913127da3947c073d3bf94b1a829aca258677a
Thommzy <tomiwaobeisun@yahoo.com>
2018-04-27 17:54:14 +0100

    chnages

--- app.js
+++ app.js
@@ -3,16 +3,28 @@ app.controller("calculatorCtrl", calc);
 
 function calc($scope) {
     $scope.display = "";
+    $scope.press=false;
 
     $scope.math = function (val){
+        if($scope.press == true){
+            clear();
+        }
+
         $scope.display += val;
     };
 
+    function clear() {
+        $scope.display="";
+        $scope.press=false;
+    }
+
     $scope.c = function (val){
         $scope.display = val;
+
     };
 
     $scope.e = function (val){
+        $scope.press=true;
         $scope.display = eval($scope.display);
     };
 
